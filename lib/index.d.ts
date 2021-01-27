ea9cc0e2d4a9b34059d1de10eb902f9a8c8449cc
UZOR <iamuzor@gmail.com>
2021-01-27 20:06:30 +0000

    Updated

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,12 @@
+/// <reference types="node" />
+import { Writable } from 'stream';
 interface DiscordStreamOption {
     webhookUrl: string;
 }
 /**
  * Pushes log streams to a Discord webhook
  */
-export declare class DiscordStream {
+export declare class DiscordStream extends Writable {
     private readonly args;
     /**
      * @param {Object} args
@@ -15,7 +17,7 @@ export declare class DiscordStream {
      * @param chunk
      * @returns {Promise}
      */
-    write(chunk: any): Promise<any>;
+    write(chunk: any): boolean;
     /**
      * Sends HTTP request to Discord webhook
      *
